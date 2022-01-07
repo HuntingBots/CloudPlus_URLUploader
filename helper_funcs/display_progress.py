e61c8ae6e09e21bc55240e9257e121b4c1e006c6
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2022-01-07 10:04:57 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -14,7 +14,7 @@ import time
 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
+    from sample_confing import Config
 else:
     from config import Config
 
